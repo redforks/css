e46e662d11028ba4bf0173ce65b9155b0ee08c7f
Red Forks <redforks@gmail.com>
2016-01-11 14:52:48 +0800

    Implement sprite command line

--- cmd/spriter/main.go
+++ cmd/spriter/main.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"flag"
-	"os"
+	"io/ioutil"
 	"path/filepath"
+
+	"github.com/redforks/css/sprite"
+	"github.com/redforks/errors"
+	"github.com/redforks/errors/cmdline"
 )
 
 func main() {
-	srcCssFile := flag.String("i", "", "Input css file")
-	dstCssFile := flag.String("o", "", "Output css file, can be the same as input css file")
-	imgBaseDiretory := flag.String("base", "", "Base directory to resolve image files referenced in input css file. Default to input css file directory")
+	cmdline.Go(func() error {
+		srcCssFile := flag.String("i", "", "Input css file")
+		dstCssFile := flag.String("o", "", "Output css file, can be the same as input css file")
+		imgBaseDiretory := flag.String("base", "", "Base directory to resolve image files referenced in input css file. Default to input css file directory")
 
-	flag.Parse()
+		flag.Parse()
 
-	if *srcCssFile == "" || *dstCssFile == "" {
-		flag.Usage()
-		os.Exit(1)
-	}
+		var (
+			css []byte
+			out string
+			err error
+		)
 
-	if *imgBaseDiretory == "" {
-		*imgBaseDiretory = filepath.Dir(*srcCssFile)
-	}
+		if *srcCssFile == "" || *dstCssFile == "" {
+			flag.Usage()
+			return cmdline.NewExitError(2)
+		}
+
+		if *imgBaseDiretory == "" {
+			*imgBaseDiretory = filepath.Dir(*srcCssFile)
+		}
+
+		if css, err = ioutil.ReadFile(*srcCssFile); err != nil {
+			return errors.NewInput(err)
+		}
+
+		spriter := sprite.New(string(css), sprite.NewFileService(*imgBaseDiretory, filepath.Dir(*dstCssFile)))
+		if out, err = spriter.Gen(); err != nil {
+			return err
+		}
+
+		if err = ioutil.WriteFile(*dstCssFile, ([]byte)(out), 0); err != nil {
+			return errors.NewRuntime(err)
+		}
+
+		return nil
+	})
 }
