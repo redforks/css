8654888004a3f53f4034d71a234e1d6207109462
Red Forks <redforks@gmail.com>
2016-11-01 21:15:15 +0800

    Put README.MD need images to gh-pages branch

--- sprite/pkg.go
+++
@@ -1,4 +0,0 @@
-// Contains Spriter struct to generate css sprite by scanning css file,
-// generate updated css file.
-// For standalone command line utility, see css/cmd/sprite package
-package sprite
--- writer/pkg.go
+++
@@ -1,2 +0,0 @@
-// writer package contains Writer struct to write out gorilla css token stream.
-package writer
